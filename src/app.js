81e9f33549895cdee102078c750dc98e3701ef02
Nicolas Requejo <nicolas.requejo@squad.com.ar>
2025-01-23 20:42:26 -0300

    add express port and view

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path')
+
+
+
+const PORT = 3000;
+
+
+app.get('/home',(req,res)=>{
+    res.sendFile(path.join(__dirname,'views/index.html'))
+})
+
+app.listen(PORT,()=> console.log('servidor ejecutandose en el puerto ' + PORT))
